2613f2fb6b41853dbde4498e04dc2fa8a62c0cf3
ArturKL <arturkalimullin10@gmail.com>
2025-07-17 00:56:55 +0300

    Fix getParcelable extension

--- app/src/main/java/com/example/androidpractice/utils/Extensions.kt
+++ app/src/main/java/com/example/androidpractice/utils/Extensions.kt
@@ -1,13 +1,14 @@
 package com.example.androidpractice.utils
 
 import android.os.Build
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import com.example.androidpractice.model.UserCredentials
 
-inline fun <reified T> Fragment.getParcelable(key: String): T? {
+inline fun <reified T : Parcelable?> Fragment.getParcelable(key: String): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        arguments?.getParcelable(Keys.CREDENTIALS_KEY, T::class.java)
+        arguments?.getParcelable(key, T::class.java)
     } else {
-        arguments?.getParcelable<UserCredentials>(Keys.CREDENTIALS_KEY) as? T
+        arguments?.getParcelable<T>(key)
     }
 }
